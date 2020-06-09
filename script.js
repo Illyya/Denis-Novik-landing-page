df5f6aab395fcf1ecaa0c59e8c003108a682c654
Illyya <62709522+Illyya@users.noreply.github.com>
2020-06-09 16:49:16 +0300

    бургер меню добавил

---
+++ script.js
@@ -0,0 +1,9 @@
+let burger = document.querySelector('.page-header__burger');
+let nav = document.querySelector('.page-header__nav-mobile');
+
+burger.onclick = function () {
+    nav.classList.toggle('active');
+}
+
+
+
